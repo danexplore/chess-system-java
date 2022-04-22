24888791a31c74e51ac0c987c6db7e58f8a30982
Daniel Moreira <danielbatistamor@gmail.com>
2022-04-22 17:09:54 -0300

    small fix

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -59,11 +59,11 @@ public class ChessMatch {
 //	Setup the pieces with placeNewPiece method
 	private void initialSetup() {
 		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-		placeNewPiece('c', 1, new King(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('d', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new King(board, Color.WHITE));
 
         placeNewPiece('c', 7, new Rook(board, Color.BLACK));
         placeNewPiece('c', 8, new Rook(board, Color.BLACK));
